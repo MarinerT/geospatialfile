eac447cdb56fcd70217b553651ff2ebcc2de571b
MarinerT <marino.todd@gmail.com>
2022-07-15 12:14:31 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Pulls GeoSpatial data from GeoBoundaries'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="geospatialfile", 
+        version=VERSION,
+        author="Todd Marino",
+        author_email="<marino_todd.com>",
+        description=DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['pycountry', 'apache-sedona', 'pyspark', 'geopandas' ],
+        keywords=['python', 'geospatial'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
