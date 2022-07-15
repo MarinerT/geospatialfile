d59b189a7c4c14d821d8bdeb70321d2c84ddccab
Todd Marino <Marino_Todd@bah.com>
2022-07-15 14:51:24 -0400

    setup update

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
-from setuptools import setup
+# from setuptools import setup
 
-if __name__ == '__main__':
-    setup()
+# if __name__ == '__main__':
+#     setup()
 
-# from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
-# VERSION = '0.0.3' 
-# DESCRIPTION = 'Pulls geospatial data from GeoBoundaries'
+VERSION = '0.0.4' 
+DESCRIPTION = 'Pulls geospatial data from GeoBoundaries'
 
-# # Setting up
-# setup(
-#        # the name must match the folder name 'verysimplemodule'
-#         name="geospatialfile", 
-#         version=VERSION,
-#         author="Todd Marino",
-#         author_email="<marino_todd.com>",
-#         description=DESCRIPTION,
-#         packages=find_packages(),
-#         install_requires=['pycountry', 'apache-sedona', 'pyspark', 'geopandas' ],
-#         keywords=['python', 'geospatial'],
-#         classifiers= [
-#             "Development Status :: 3 - Alpha",
-#             "Intended Audience :: Education",
-#             "Programming Language :: Python :: 2",
-#             "Programming Language :: Python :: 3",
-#             "Operating System :: MacOS :: MacOS X",
-#             "Operating System :: Microsoft :: Windows",
-#         ]
-# )
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="geospatialfile", 
+        version=VERSION,
+        author="Todd Marino",
+        author_email="<marino_todd.com>",
+        description=DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['pycountry', 'apache-sedona', 'pyspark', 'geopandas' ],
+        keywords=['python', 'geospatial'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
